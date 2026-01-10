c929e43d0aa9e625c12cd73edc29f84242f2d2c5
Uchiyama Yuto <uchiyama.yuto.m1@elms.hokudai.ac.jp>
2026-01-10 18:14:18 +0900

    todosの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,65 @@
 // React から useState フックをインポート
 import { useState } from 'react';
 
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
   
   // 初期値: 空文字列 ''
   const [text, setText] = useState('');
   const [todos,setTodos] = useState<Todo[]>([]);
 
+   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+   };
+
+  // todos ステートを更新する関数
+  const handleSubmit = () => {
+    // 何も入力されていなかったらリターン
+    if (!text) return;
+
+    // 新しい Todo を作成
+    // 明示的に型注釈を付けてオブジェクトの型を限定する
+    const newTodo: Todo = {
+      // text ステートの値を value プロパティへ
+      value: text,
+    };
+
+     
+  
+
+    /**
+     * 更新前の todos ステートを元に
+     * スプレッド構文で展開した要素へ
+     * newTodo を加えた新しい配列でステートを更新
+     **/
+    setTodos((todos) => [newTodo, ...todos]);
+    // フォームへの入力をクリアする
+    setText('');
+  };
+
+  
+
   return (
+    
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form
+       onSubmit={(e) =>{ 
+        e.preventDefault();
+        handleSubmit();
+      }}
+    >
         <input
           type="text"
           // text ステートが持っている入力中テキストの値を value として表示
           value={text}
           // onChange イベント（＝入力テキストの変化）を text ステートに反映する
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => handleChange(e)}
         />
-        <input type="submit" />  {/* ← 省略 */}
+        <input type="submit" value='追加'　onSubmit={handleSubmit}  />  {/* ← 省略 */}
       </form>
-
-      {/* ↓ DOM のリアクティブな反応を見るためのサンプル */}
-      <p>{text}</p>
-      {/* ↑ あとで削除 */}
     </div>
   );
 };
